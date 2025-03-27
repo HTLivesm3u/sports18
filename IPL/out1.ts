7e3a7a3ff8cd1d1bfabeda58a927bd54169443b1
unkown_4777 <169054920+HTLivesm3u@users.noreply.github.com>
2025-03-27 18:14:20 +0530

    Update out1.ts

--- IPL/out1.ts
+++ IPL/out1.ts
@@ -1,7 +1,7 @@
 #EXTM3U
-#EXT-X-TARGETDURATION:
+#EXT-X-TARGETDURATION:10
 #EXT-X-VERSION:3
 #EXTINF:10,
-https://dj-tm-srvr1.vercel.app/title/STAR_SPORTS_1_HINDI-HD@DJ-TM.mpd?data=0y7KCy8MSQ0IKLTMtdQvyk9P8QwpKzX2DQnMzSnwdAs3dcv2qkgtC_EJcw4LdDGzMI3SNgvMMDHIqyrx8ysM8_DxSnHMck8sK660DHLx9gvM8ksKyE4qcAQA%7CCookie=hdntl=exp=1742995589~acl=*sshindi*~id=b0442a2df488ac84ef378622052139f0~data=hdntl~hmac=9b6a699c0afe7180549ee48744b066aa415804fb77ffe23956bac560e5bcb7b4&User-Agent=Hotstar;in.startv.hotstar/25.02.24.8.11169%20(Android/15)&Telegram=@links_macha_official&Creator=@DJ-TM&drmScheme=clearkey&drmLicense=_ncY-7P7S6eMB8wPV4dE5g:Yk4ksYQ7RZ-rCpSWCUFvDQ
+https://livetvbox.live:443/live/rajnikant/rajnikant/98866.ts
 #EXTINF:10,
-https://dj-tm-srvr1.vercel.app/title/STAR_SPORTS_1_HINDI-HD@DJ-TM.mpd?data=0y7KCy8MSQ0IKLTMtdQvyk9P8QwpKzX2DQnMzSnwdAs3dcv2qkgtC_EJcw4LdDGzMI3SNgvMMDHIqyrx8ysM8_DxSnHMck8sK660DHLx9gvM8ksKyE4qcAQA%7CCookie=hdntl=exp=1742995589~acl=*sshindi*~id=b0442a2df488ac84ef378622052139f0~data=hdntl~hmac=9b6a699c0afe7180549ee48744b066aa415804fb77ffe23956bac560e5bcb7b4&User-Agent=Hotstar;in.startv.hotstar/25.02.24.8.11169%20(Android/15)&Telegram=@links_macha_official&Creator=@DJ-TM&drmScheme=clearkey&drmLicense=_ncY-7P7S6eMB8wPV4dE5g:Yk4ksYQ7RZ-rCpSWCUFvDQ
+https://livetvbox.live:443/live/rajnikant/rajnikant/98866.ts
