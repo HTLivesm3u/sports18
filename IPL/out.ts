423cd6f5f6f153a5fb9ee0bd7716ca12ef4ad2c7
unkown_4777 <169054920+HTLivesm3u@users.noreply.github.com>
2025-03-30 11:29:28 +0530

    Update out.ts

--- IPL/out.ts
+++ IPL/out.ts
@@ -1,9 +1,8 @@
 #EXTM3U
-
 #EXT-X-VERSION:3
 
 
-#EXTINF:10,
+#EXTINF:,
 https://dj-tm-srvr2.vercel.app/thrd_prty/hls/live/star-sports-1-hindi-hd@DJ-TM.ts?ch_id=63560%7CTelegram=@links_macha_official&Creator=@DJ-TM
-#EXTINF:10,
+#EXTINF:,
 https://dj-tm-srvr2.vercel.app/thrd_prty/hls/live/star-sports-1-hindi-hd@DJ-TM.ts?ch_id=63560%7CTelegram=@links_macha_official&Creator=@DJ-TM
