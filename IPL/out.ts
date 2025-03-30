c855d06b09e6f35728978fd1f89b4c2bcd548167
unkown_4777 <169054920+HTLivesm3u@users.noreply.github.com>
2025-03-30 11:23:29 +0530

    Update out.ts

--- IPL/out.ts
+++ IPL/out.ts
@@ -4,6 +4,6 @@
 #EXT-X-MEDIA-SEQUENCE:89027
 
 #EXTINF:10,
-https://dj-tm-srvr2.vercel.app/thrd_prty/hls/live/star-sports-1-hindi-hd@DJ-TM.ts?ch_id=63560
+https://dj-tm-srvr2.vercel.app/thrd_prty/hls/live/star-sports-1-hindi-hd@DJ-TM.ts?ch_id=63560%7CTelegram=@links_macha_official&Creator=@DJ-TM
 #EXTINF:10,
 https://livetvbox.live:443/live/rajnikant/rajnikant/98866.ts
