01ba11ff279348c865c01c54406ecde67c42623c
unkown_4777 <169054920+HTLivesm3u@users.noreply.github.com>
2025-03-30 11:19:39 +0530

    Update out.ts

--- IPL/out.ts
+++ IPL/out.ts
@@ -1,5 +1,5 @@
 #EXTM3U
 #EXT-X-STREAM-INF:BANDWIDTH=1500000,RESOLUTION=1280x720
-https://myctxa.temycssme.wiki/720p.m3u8
+https://dj-tm-srvr2.vercel.app/thrd_prty/hls/live/star-sports-1-hindi-hd@DJ-TM.ts?ch_id=63560|Telegram=@links_macha_official&Creator=@DJ-TM
 #EXT-X-STREAM-INF:BANDWIDTH=3000000,RESOLUTION=1920x1080
-https://allinonereborn.live/test.m3u8?id=65058
+https://dj-tm-srvr2.vercel.app/thrd_prty/hls/live/star-sports-1-hindi-hd@DJ-TM.ts?ch_id=63560|Telegram=@links_macha_official&Creator=@DJ-TM
