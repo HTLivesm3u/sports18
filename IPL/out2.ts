3d3fcd36f44aed2b5d7dae8f3e77c1d032976575
unkown_4777 <169054920+HTLivesm3u@users.noreply.github.com>
2025-04-20 12:59:58 +0530

    Update out2.ts

--- IPL/out2.ts
+++ IPL/out2.ts
@@ -1,17 +1,13 @@
 #EXTM3U
 #EXT-X-VERSION:3
-#EXT-X-MEDIA-SEQUENCE:6856
-#EXT-X-ALLOW-CACHE:YES
-#EXT-X-TARGETDURATION:2
-#EXTINF:1.160000,
+#EXT-X-TARGETDURATION:10
+#EXT-X-MEDIA-SEQUENCE:63537
+#EXTINF:10, FRAME-RATE=60.0, AUDIO="5.1", CODECS="avc1.640028,ec-3.1.1,mp4a.40.5",
 http://starshare.org:80/live/love95/love95/213049.ts
-#EXTINF:1.160000,
+#EXTINF:10, FRAME-RATE=60.0, AUDIO="5.1", CODECS="avc1.640028,ec-3.1.1,mp4a.40.5",
 http://starshare.org:80/live/love95/love95/213049.ts
-#EXTINF:1.160000,
+#EXT-X-DISCONTINUITY
+#EXTINF:10, FRAME-RATE=60.0, AUDIO="5.1", CODECS="avc1.640028,ec-3.1.1,mp4a.40.5",
 http://starshare.org:80/live/love95/love95/213049.ts
-#EXTINF:1.160000,
-http://starshare.org:80/live/love95/love95/213049.ts
-#EXTINF:1.160000,
-http://starshare.org:80/live/love95/love95/213049.ts
-#EXTINF:1.160000,
+52/63541#EXTINF:10, FRAME-RATE=60.0, AUDIO="5.1", CODECS="avc1.640028,ec-3.1.1,mp4a.40.5",
 http://starshare.org:80/live/love95/love95/213049.ts
