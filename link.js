e611f18f3524ce00736b5f9cfa3ce86d382d4bef
unkown_4777 <169054920+HTLivesm3u@users.noreply.github.com>
2026-01-06 22:41:47 +0530

    Update VIDEO_LINK to new streaming URL

--- link.js
+++ link.js
@@ -1,3 +1,3 @@
 // Sirf yaha link change karna padega
-const VIDEO_LINK = "https://allinonereborn.xyz/push4k2/index.php?id=22148";
+const VIDEO_LINK = "http://starshare.net:80/live/Suryaaa/SURYAAAA/98866.ts";
 
