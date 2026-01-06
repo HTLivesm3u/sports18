9fe034b5225a1daf2a6c25ad3d53001998dfb2a4
unkown_4777 <169054920+HTLivesm3u@users.noreply.github.com>
2026-01-06 22:28:36 +0530

    Update VIDEO_LINK to new streaming URL

--- link.js
+++ link.js
@@ -1,3 +1,3 @@
 // Sirf yaha link change karna padega
-const VIDEO_LINK = "https://tvsen5.aynaott.com/c9EpzZ6fQBJ3/index.m3u8";
+const VIDEO_LINK = "https://allinonereborn.xyz/filex.m3u8/live2.php?id=520366";
 
