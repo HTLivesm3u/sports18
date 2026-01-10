d7e1edf1ccd557c66a18b6d488d66a708540c8a7
unkown_4777 <169054920+HTLivesm3u@users.noreply.github.com>
2026-01-10 17:12:56 +0530

    Update link.js

--- link.js
+++ link.js
@@ -1,3 +1,3 @@
 // Sirf yaha link change karna padega
-const VIDEO_LINK = "https://allinonereborn.xyz/trx.m3u8/live3.php?id=117723";
+const VIDEO_LINK = "https://dillzy.cricketstream745.workers.dev/live.m3u8";
 
