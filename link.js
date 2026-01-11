c5dc4f15311f2b6bde5194b7a200a6817258c90c
unkown_4777 <169054920+HTLivesm3u@users.noreply.github.com>
2026-01-11 15:51:34 +0530

    Update link.js

--- link.js
+++ link.js
@@ -1,3 +1,3 @@
 // Sirf yaha link change karna padega
-const VIDEO_LINK = "https://tvsen5.aynascope.net/c9EpzZ6fQBJ3/index.m3u8?e=1768105894&u=af86ae3e-e545-4562-9d47-82c5c1447743&token=927aeb2187e27d37679883d32a7cc130";
+const VIDEO_LINK = "https://shotner.u0k.workers.dev/Hindi.m3u8";
 
