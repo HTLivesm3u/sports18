6e9fba2ce4558e55f196c589a81eac7b5a29d698
unkown_4777 <169054920+HTLivesm3u@users.noreply.github.com>
2026-01-18 19:21:22 +0530

    Update link.js

--- link.js
+++ link.js
@@ -1,3 +1,3 @@
 // Sirf yaha link change karna padega
-const VIDEO_LINK = "https://tvsen5.aynascope.net/cXPB2LKkErN9/index.m3u8?e=1768733755&u=af86ae3e-e545-4562-9d47-82c5c1447743&token=5cc8e01ee39e4ce576795b77853266dd";
+const VIDEO_LINK = "https://dillzyserver.dillzycreations.workers.dev/live/hotstar.m3u8";
 
