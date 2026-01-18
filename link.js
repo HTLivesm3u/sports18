f6c2f12eca85c42c5910359f9960da4fa7797b97
unkown_4777 <169054920+HTLivesm3u@users.noreply.github.com>
2026-01-18 23:57:03 +0530

    Update video link in link.js

--- link.js
+++ link.js
@@ -1,3 +1,3 @@
 // Sirf yaha link change karna padega
-const VIDEO_LINK = "https://dillzyserver.dillzycreations.workers.dev/live/hotstar.m3u8";
+const VIDEO_LINK = "https://jioweb5153.techraj.site/app/live.php?id=1984";
 
