b845fe8eea79b7d10ed2b4765d4cfd3273075223
unkown_4777 <169054920+HTLivesm3u@users.noreply.github.com>
2026-01-25 00:57:19 +0530

    Update link.js

--- link.js
+++ link.js
@@ -1,4 +1,4 @@
 // Sirf yaha link change karna padega
 //const VIDEO_LINK = "https://jioweb5153.techraj.site/app/live.php?id=1984";
-const VIDEO_LINK = "https://v5on.site/cchd.m3u8?ch=starhindi";
+const VIDEO_LINK = "https://d15.epicquesthero.com:999/hls/starhindi.m3u8?md5=4V9liYos9jXPMvNSDUoteQ&expires=1769289731|Referer=https://vividmosaica.com";
 
