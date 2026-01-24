b63fe3f09ad4c7c6dcf8358559d92fe2088c68ae
unkown_4777 <169054920+HTLivesm3u@users.noreply.github.com>
2026-01-25 01:01:10 +0530

    Update link.js

--- link.js
+++ link.js
@@ -1,4 +1,4 @@
 // Sirf yaha link change karna padega
 //const VIDEO_LINK = "https://jioweb5153.techraj.site/app/live.php?id=1984";
-const VIDEO_LINK = "https://bd-streams.site/a8bfa3d2-2ff2-4f4a-bae0-990d209fd3d3";
+const VIDEO_LINK = "https://s1.v5on.site/live/api/playlist.php?id=276731";
 
