95f841ecba9833b507c06e794ba4c4ec527caea8
unkown_4777 <169054920+HTLivesm3u@users.noreply.github.com>
2026-01-25 18:53:34 +0530

    Update link.js

--- link.js
+++ link.js
@@ -1,5 +1,5 @@
 // Sirf yaha link change karna padega
 // //const VIDEO_LINK = "https://jioweb5153.techraj.site/app/live.php?id=1984";
  //const VIDEO_LINK = "https://crichd.workspace-sultanarabi161.workers.dev/live/willowusa.m3u8";
-const VIDEO_LINK = "https://live.dinesh29.com.np/stream/jiotvplus/starsportshd1hindi/master.m3u8";
+const VIDEO_LINK = "https://hotstarlive.vercel.app/HIN.m3u8";
 
