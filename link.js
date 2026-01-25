babde15de7a211a32125151523924202d7d13e0d
unkown_4777 <169054920+HTLivesm3u@users.noreply.github.com>
2026-01-25 12:24:05 +0530

    Update link.js

--- link.js
+++ link.js
@@ -1,4 +1,4 @@
 // Sirf yaha link change karna padega
 //const VIDEO_LINK = "https://jioweb5153.techraj.site/app/live.php?id=1984";
-const VIDEO_LINK = "https://s1.v5on.site/live/api/playlist.php?id=276731";
+const VIDEO_LINK = "https://dillzy.xo.je/mac_single_script/index.php?stream_url=http%3A%2F%2Fexa_edge_2skyd.cdn77.me%2F58eab44776cce29a3ffa1c244a8252f0300f4db2ae63cd48c3b4769ef8559b2c%2Fmono.m3u8%3Ftoken%3Dfe6ecedea3fecda7c432e90652fbf9ac";
 
