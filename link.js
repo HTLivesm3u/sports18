33ad685705ee2f183df485dc243970840920db8f
unkown_4777 <169054920+HTLivesm3u@users.noreply.github.com>
2026-01-25 15:30:16 +0530

    Update link.js

--- link.js
+++ link.js
@@ -1,5 +1,5 @@
 // Sirf yaha link change karna padega
 // //const VIDEO_LINK = "https://jioweb5153.techraj.site/app/live.php?id=1984";
- const VIDEO_LINK = "https://crichd.workspace-sultanarabi161.workers.dev/live/willowusa.m3u8";
-//const VIDEO_LINK = "https://tvsen5.aynascope.net/c9EpzZ6fQBJ3/index.m3u8?e=1769329067&u=78be6644-0a65-48ec-81a4-089ac65a2619&token=8df70cea98a65619d6776283a0e16dce";
+ //const VIDEO_LINK = "https://crichd.workspace-sultanarabi161.workers.dev/live/willowusa.m3u8";
+const VIDEO_LINK = "https://tvsen5.aynascope.net/cXPB2LKkErN9/index.m3u8?e=1769336135&u=78be6644-0a65-48ec-81a4-089ac65a2619&token=fa98cfc4650f16c78e07e8623a54bf44";
 
