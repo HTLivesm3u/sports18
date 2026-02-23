15086e9b019df1c8f52b7bfe06bab88421c06f79
unkown_4777 <169054920+HTLivesm3u@users.noreply.github.com>
2026-02-23 22:46:39 +0530

    Update link.js

--- link.js
+++ link.js
@@ -1,5 +1,5 @@
 // Sirf yaha link change karna padega
 // //const VIDEO_LINK = "https://jioweb5153.techraj.site/app/live.php?id=1984";
  //const VIDEO_LINK = "https://crichd.workspace-sultanarabi161.workers.dev/live/willowusa.m3u8";
-const VIDEO_LINK = "https://amg01269-amg01269c1-sportstribal-emea-5204.playouts.now.amagi.tv/ts-eu-w1-n2/playlist/amg01269-willowtvfast-willowplus-sportstribalemea/playlist.m3u8";
+const VIDEO_LINK = "http://tvn1.chowdhury-shaheb.com/starsport1/index.m3u8";
 
